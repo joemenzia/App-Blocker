7cb182155df21553872b9b680d5908a46129363e
joemenzia <joe.menzia@gmail.com>
2025-08-21 14:53:01 -0700

    now connected to github

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Let's go!</Text>
+      <Text>Now it works!</Text>
       <StatusBar style="auto" />
     </View>
   );
